ce6b4819fd97d718aa970f6371747fffef1d1dc7
yacenkonika <yacenkonika05@mail.ru>
2025-04-26 13:56:30 +0300

    с

--- framework/modules/auth_basic.php
+++ framework/modules/auth_basic.php
@@ -5,16 +5,20 @@
 
 function auth(&$request, $r) {
   // TODO: запрашивать пользователя из БД.
-  $users = array(
+  $users = array(//хранение паролей, но надо заменить на получение из бд
     'admin' => '123',
   );
+  //empty($users)Проверяет, установлена ли переменная $user.
+  //  Это позволяет не переопределять пользователя, если он уже был аутентифицирован ранее.
   if (empty($user) && !empty($_SERVER['PHP_AUTH_USER'])) {
-    $user = array(
+    $user = array(//массив с логином и паролем
       'login' => $_SERVER['PHP_AUTH_USER'],
       'pass' => $users[$_SERVER['PHP_AUTH_USER']]
     );
-    $request['user'] = $user;
+    $request['user'] = $user;//Информация о пользователе добавляется в массив $request, 
+    // чтобы она была доступна другим частям фреймворка.
   }
+  //следующий блок кода проверяет, прошла ли аутентификация успешно.
   if (!isset($_SERVER['PHP_AUTH_USER']) || empty($user) || $_SERVER['PHP_AUTH_USER'] != $user['login'] || $_SERVER['PHP_AUTH_PW'] != $user['pass']) {
     unset($user);
     $response = array(
