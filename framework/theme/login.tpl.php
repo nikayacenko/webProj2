295699be1375c7e5facb318ec6be4cafaf1bb970
anitaloz <anitalozova@gmail.com>
2025-05-01 22:46:47 +0300

    c

---
+++ framework/theme/login.tpl.php
@@ -0,0 +1,17 @@
+<div class="content container-fluid mt-sm-0">
+      <div class="log">
+        <form action="' . url('login') . '" method="post">
+          <label>
+            Логин:<br />
+            <input name="login" />
+          </label><br />
+          <label>
+            Пароль:<br />
+            <input name="pass" type="password" />
+          </label><br />
+          <input type="submit" value="Войти" />
+          <input type="hidden" name="csrf_token" value="' . $csrf_token . '">
+        </form>
+        <a href="' . url('adm_page') . '">Вход для администратора</a>
+      </div>
+    </div>';
