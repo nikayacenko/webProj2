6317838faa2f7fb0698a997823790e7386819c62
anitaloz <anitalozova@gmail.com>
2025-05-01 16:30:06 +0300

    с

--- framework/modules/front.php
+++ framework/modules/front.php
@@ -8,13 +8,7 @@
 // Обработчик запросов методом GET.
 function front_get($request) {
   // Пример ответа веб-сервиса.
-  return array('headers' => array('Content-Type' => 'application/xml'), 'entity' => '<document />');
-  // Пример возврата контента.
-  return '123';
-  // Пример запрета доступа.
-  return access_denied();
-  // Пример возврата ресурс не найден.
-  return not_found();
+  return theme('form');
 }
 
 // Обработчик запросов методом POST.
