8056e2ae2290087d832c16066dfaa5ab829ffc59
anitaloz <anitalozova@gmail.com>
2025-05-01 22:46:44 +0300

    c

--- framework/modules/login.php
+++ framework/modules/login.php
@@ -11,27 +11,8 @@ function login_get($request, $db) {
   // Генерация CSRF token
   $csrf_token = htmlspecialchars(generateCsrfToken());
 
-  // Формируем HTML-код формы входа
-  $form = '
-    <div class="content container-fluid mt-sm-0">
-      <div class="log">
-        <form action="' . url('login') . '" method="post">
-          <label>
-            Логин:<br />
-            <input name="login" />
-          </label><br />
-          <label>
-            Пароль:<br />
-            <input name="pass" type="password" />
-          </label><br />
-          <input type="submit" value="Войти" />
-          <input type="hidden" name="csrf_token" value="' . $csrf_token . '">
-        </form>
-        <a href="' . url('adm_page') . '">Вход для администратора</a>
-      </div>
-    </div>';
-
-  return $form; // Возвращаем HTML-код формы
+  // Формируем HTML-код формы вход;
+    return theme('login', $csrf_token);
 }
 
 
