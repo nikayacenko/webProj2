a684cffc2c2b63b606012c1a458b1c7c2f994b2c
anitaloz <anitalozova@gmail.com>
2025-05-01 18:55:48 +0300

    c

--- framework/scripts/db.php
+++ framework/scripts/db.php
@@ -1,9 +1,9 @@
 <?php
 
 global $db;
-$user = 'u68598'; // Заменить на ваш логин uXXXXX
-$pass = '8795249'; // Заменить на пароль
-$db = new PDO('mysql:host=localhost;dbname=u68598', $user, $pass,
+$user = 'u68600'; // Заменить на ваш логин uXXXXX
+$pass = '8589415'; // Заменить на пароль
+$db = new PDO('mysql:host=localhost;dbname=u68600', $user, $pass,
 [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
 
 
