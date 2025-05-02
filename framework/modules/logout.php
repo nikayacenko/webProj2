39a4f72b30859c5c5b3baa1c880bb096500d6154
anitaloz <anitalozova@gmail.com>
2025-05-02 15:07:41 +0300

    c

--- framework/modules/logout.php
+++ framework/modules/logout.php
@@ -1,14 +1,22 @@
 <?php
    function logout_post($request, $db) {
        if ($request['method'] == 'post' && isset($request['post']['logout'])) {
-           session_start(); // Возобновляем сессию, если она еще не активна
-
-           $_SESSION = array();// очищаем переменные сессии.
-           // Уничтожаем сессию.
+           session_unset()
            session_destroy();
-           setcookie(session_name(), '', time() - 3600, "/"); // Удаляем куки сессии
-
+           //setcookie(session_name(), '', time() - 3600, "/"); // Удаляем куки сессии
            return redirect('./'); // Перенаправляем на главную
        }
    }
 ?>
+
+if (!empty($_SESSION['login'])) {
+    if(isset($_POST['logout'])){
+      session_unset();
+      session_destroy();
+      header('Location: login.php');
+      exit();
+    }
+    header('Location: ./');
+    exit();
+  }
+}
