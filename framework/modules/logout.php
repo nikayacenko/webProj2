d5ab8fb4b26653ad70ec4a703dacc6ed20e9bead
anitaloz <anitalozova@gmail.com>
2025-05-02 15:09:41 +0300

    c

--- framework/modules/logout.php
+++ framework/modules/logout.php
@@ -1,4 +1,6 @@
 <?php
+require_once './scripts/db.php';
+global $db;
    function logout_post($request, $db) {
        if ($request['method'] == 'post' && isset($request['post']['logout'])) {
            session_unset();
@@ -8,15 +10,3 @@
        }
    }
 ?>
-
-if (!empty($_SESSION['login'])) {
-    if(isset($_POST['logout'])){
-      session_unset();
-      session_destroy();
-      header('Location: login.php');
-      exit();
-    }
-    header('Location: ./');
-    exit();
-  }
-}
