21a9294f19ac3cfab208eb232d432d363b03a30b
anitaloz <anitalozova@gmail.com>
2025-05-02 15:12:53 +0300

    c

--- framework/modules/logout.php
+++ framework/modules/logout.php
@@ -2,7 +2,9 @@
 require_once './scripts/db.php';
 global $db;
    function logout_post($request, $db) {
+    var_dump("xxx");
        if ($request['method'] == 'post' && isset($request['post']['logout'])) {
+        var_dump("yyy");
            session_unset();
            session_destroy();
            //setcookie(session_name(), '', time() - 3600, "/"); // Удаляем куки сессии
