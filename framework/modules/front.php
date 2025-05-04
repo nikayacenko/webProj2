2aed8069d9639471fb2f4d13cfb10078621ad181
yacenkonika <yacenkonika05@mail.ru>
2025-05-04 18:20:58 +0300

    c

--- framework/modules/front.php
+++ framework/modules/front.php
@@ -209,12 +209,12 @@ function front_get($request, $db) {
 
 
 
-function generateCsrfToken() {
-  if (empty($_SESSION['csrf_token'])) {
-      $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-  }
-  return $_SESSION['csrf_token'];
-}
+// function generateCsrfToken() {
+//   if (empty($_SESSION['csrf_token'])) {
+//       $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+//   }
+//   return $_SESSION['csrf_token'];
+// }
 
 function validateCsrfToken() {
   if (!isset($_POST['csrf_token']) || !isset($_SESSION['csrf_token']) || !hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
