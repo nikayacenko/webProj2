49c440175d60f5ba29c706d47e675dae5d25c488
yacenkonika <yacenkonika05@mail.ru>
2025-05-04 18:19:26 +0300

    c

--- framework/modules/front.php
+++ framework/modules/front.php
@@ -209,18 +209,37 @@ function front_get($request, $db) {
 
 
 
+function generateCsrfToken() {
+  if (empty($_SESSION['csrf_token'])) {
+      $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+  }
+  return $_SESSION['csrf_token'];
+}
 
+function validateCsrfToken() {
+  if (!isset($_POST['csrf_token']) || !isset($_SESSION['csrf_token']) || !hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
+      return false;
+  }
+  return true;
+}
+
+function sanitizeString($string) {
+return htmlspecialchars(strip_tags($string), ENT_QUOTES, 'UTF-8');
+}
 
 
 // Обработчик запросов методом POST.
 function front_post($request, $db) {
   // Пример возврата редиректа.
+  $isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+  if ($isAjax) {
+    header('Content-Type: application/json');
+}
   if (!validateCsrfToken()) {
     http_response_code(403);
     echo "CSRF token validation failed."; // Опционально, сообщение об ошибке
     exit; // Обязательно останавливаем выполнение скрипта
-}
-  $isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+  }
 
   $messages = [];
 
@@ -340,8 +359,7 @@ if (!empty($request['post']['bio']) && !preg_match('/^[а-яА-Яa-zA-Z1-9.,?!:(
 setcookie('bio_value', htmlspecialchars($request['post']['bio'], ENT_QUOTES, 'UTF-8'), time() + 365 * 24 * 60 * 60);
 
 if ($isAjax) {
-  header('Content-Type: application/json');
-  echo json_encode([
+  $response = [
       'success' => !$errors,
       'messages' => $messages,
       'errors' => [
@@ -366,7 +384,8 @@ if ($isAjax) {
           'bio' => $_COOKIE['bio_value'] ?? '',
           // ... остальные поля
       ]
-  ]);
+  ];
+  echo json_encode($response);
   exit();
 }
 
