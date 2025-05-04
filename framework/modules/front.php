dcb7e2db750d0d04ea1805be87f4beecd119e607
yacenkonika <yacenkonika05@mail.ru>
2025-05-04 18:23:17 +0300

    c

--- framework/modules/front.php
+++ framework/modules/front.php
@@ -209,37 +209,21 @@ function front_get($request, $db) {
 
 
 
-// function generateCsrfToken() {
-//   if (empty($_SESSION['csrf_token'])) {
-//       $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-//   }
-//   return $_SESSION['csrf_token'];
-// }
 
-// function validateCsrfToken() {
-//   if (!isset($_POST['csrf_token']) || !isset($_SESSION['csrf_token']) || !hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
-//       return false;
-//   }
-//   return true;
-// }
-
-function sanitizeString($string) {
-return htmlspecialchars(strip_tags($string), ENT_QUOTES, 'UTF-8');
-}
 
 
 // Обработчик запросов методом POST.
 function front_post($request, $db) {
-  // Пример возврата редиректа.
   $isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
   if ($isAjax) {
     header('Content-Type: application/json');
-}
-  if (!validateCsrfToken()) {
-    http_response_code(403);
-    echo "CSRF token validation failed."; // Опционально, сообщение об ошибке
-    exit; // Обязательно останавливаем выполнение скрипта
   }
+  // Пример возврата редиректа.
+  if (!validateCsrfToken()) {
+        http_response_code(403);
+        echo "CSRF token validation failed."; // Опционально, сообщение об ошибке
+        exit; // Обязательно останавливаем выполнение скрипта
+    }
 
   $messages = [];
 
@@ -359,7 +343,7 @@ if (!empty($request['post']['bio']) && !preg_match('/^[а-яА-Яa-zA-Z1-9.,?!:(
 setcookie('bio_value', htmlspecialchars($request['post']['bio'], ENT_QUOTES, 'UTF-8'), time() + 365 * 24 * 60 * 60);
 
 if ($isAjax) {
-  $response = [
+  echo json_encode([
       'success' => !$errors,
       'messages' => $messages,
       'errors' => [
@@ -384,8 +368,7 @@ if ($isAjax) {
           'bio' => $_COOKIE['bio_value'] ?? '',
           // ... остальные поля
       ]
-  ];
-  echo json_encode($response);
+  ]);
   exit();
 }
 
