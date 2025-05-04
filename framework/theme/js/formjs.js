b92dc022296e174cc6bc1e967d42a69f052aa6d3
yacenkonika <yacenkonika05@mail.ru>
2025-05-04 17:53:02 +0300

    c

--- framework/theme/js/formjs.js
+++ framework/theme/js/formjs.js
@@ -18,21 +18,21 @@ document.getElementById('myform').addEventListener('submit', async (e) => {
         const data = await response.json();
 
         // 1. Обновляем сообщения
-        let messagesDiv = document.getElementById('messages');
-        if (!messagesDiv) {
-            messagesDiv = document.createElement('div');
-            messagesDiv.id = 'messages';
-            form.prepend(messagesDiv);
-        }
-        messagesDiv.innerHTML = data.messages?.join('<br>') || '';
+        // let messagesDiv = document.getElementById('messages');
+        // if (!messagesDiv) {
+        //     messagesDiv = document.createElement('div');
+        //     messagesDiv.id = 'messages';
+        //     form.prepend(messagesDiv);
+        // }
+        // messagesDiv.innerHTML = data.messages?.join('<br>') || '';
 
-        // 2. Подсвечиваем ошибки
-        if (data.errors) {
-            Object.keys(data.errors).forEach(field => {
-                const input = form.querySelector([name="${CSS.escape(field)}"]);
-                if (input) input.classList.toggle('error', !!data.errors[field]);
-            });
-        }
+        // // 2. Подсвечиваем ошибки
+        // if (data.errors) {
+        //     Object.keys(data.errors).forEach(field => {
+        //         const input = form.querySelector([name="${CSS.escape(field)}"]);
+        //         if (input) input.classList.toggle('error', !!data.errors[field]);
+        //     });
+        // }
 
         // 3. Восстанавливаем значения (если нужно)
         if (data.values) {
