e771df7e1939ef05abd260739a6c7dea4ca64dab
anitaloz <anitalozova@gmail.com>
2025-05-05 13:42:02 +0300

    c

--- framework/modules/front.php
+++ framework/modules/front.php
@@ -214,6 +214,8 @@ function front_get($request, $db) {
 
 // Обработчик запросов методом POST.
 function front_post($request, $db) {
+  $isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) && 
+             $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest';
   // Пример возврата редиректа.
   if (!validateCsrfToken()) {
     http_response_code(403); 
@@ -443,6 +445,7 @@ else {
 setcookie('save', '1');
 
 // Делаем перенаправление.
+if()
   return redirect();
 }
 
