c67290e6d037495b504093a93ec02b169000e7d1
yacenkonika <yacenkonika05@mail.ru>
2025-05-05 19:30:08 +0300

    c

--- framework/modules/front.php
+++ framework/modules/front.php
@@ -337,11 +337,11 @@ if (!empty($request['post']['bio']) && !preg_match('/^[а-яА-Яa-zA-Z1-9.,?!:(
 setcookie('bio_value', htmlspecialchars($request['post']['bio'], ENT_QUOTES, 'UTF-8'), time() + 365 * 24 * 60 * 60);
 
 
-if ($isAjax) {
-  header('Content-Type: application/json');
-  echo json_encode(['success' => true]);
-  exit;
-}
+// if ($isAjax) {
+//   header('Content-Type: application/json');
+//   echo json_encode(['success' => true]);
+//   exit;
+// }
 // Обработка AJAX-запроса
 // if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
 //   header('Content-Type: application/json');
