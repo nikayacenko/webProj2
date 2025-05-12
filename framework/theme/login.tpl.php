a18363e60c34c46ca246eb699774a997b01e6562
nikayacenko <yacenkonika05@mail.ru>
2025-05-12 10:16:03 +0300

    Update login.tpl.php

--- framework/theme/login.tpl.php
+++ framework/theme/login.tpl.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="theme/js/slick.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="theme/style.css">
+    <script src="theme/js/second-slider.js"></script>
+    <link rel="icon" href="theme/img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="theme/img/favicon.ico"  type="image/x-icon">
+    <title>Project</title>
+</head>
 <div class="content container-fluid mt-sm-0">
   <div class="log">
     <form action="<?php echo url('login'); ?>" method="post">
