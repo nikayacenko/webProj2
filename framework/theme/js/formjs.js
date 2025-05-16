618fcc45a85367dbe8ba2d58ead0fb1b186f7c37
anitaloz <anitalozova@gmail.com>
2025-05-16 19:34:24 +0300

    c

--- framework/theme/js/formjs.js
+++ framework/theme/js/formjs.js
@@ -592,7 +592,7 @@ function highlightError(element, message) {
         element.parentNode.insertBefore(errorElement, element.nextSibling);
         if(element.type==='radio')
         {
-            element.parentNode.parentNode.insertBefore(errorElement, element.parentElement);
+            element.parentNode.parentNode.parentNode.insertBefore(errorElement, element.parentElement.parentElement);
         }
         if(element.type==='checkbox')
         {
