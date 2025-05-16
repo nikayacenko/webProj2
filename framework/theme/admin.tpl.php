27935759c845f8920da2073bd3745cc5b7b97585
anitaloz <anitalozova@gmail.com>
2025-05-16 22:19:26 +0300

    c

--- framework/theme/admin.tpl.php
+++ framework/theme/admin.tpl.php
@@ -61,7 +61,7 @@
 
         <?php
         try {
-            echo "<table class='stat'><tr class='nametb px-sm-2 pt-sm-2 pb-sm-2'><td>LANGUAGE</td><td>COUNT</td></tr>";
+            echo "<table class='stat'><tr class='nametb px-sm-2 pt-sm-2 pb-sm-2'><td class='tdstat'>LANGUAGE</td><td class='tdstat'>COUNT</td></tr>";
             foreach($stat as $row){
               echo "<tr><td class='tdstat'>" . htmlspecialchars($row['namelang'], ENT_QUOTES, 'UTF-8'). "</td><td>" . htmlspecialchars($row['cnt'], ENT_QUOTES, 'UTF-8') . "</td></tr>";
           }
