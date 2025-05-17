7764e4ae0e4ddc407f211c1ebde258651315126e
yacenkonika <yacenkonika05@mail.ru>
2025-05-17 19:37:56 +0300

    p

--- framework/modules/front.php
+++ framework/modules/front.php
@@ -9,8 +9,7 @@ global $db;
 // Обработчик запросов методом GET.
 require_once './scripts/db.php';
 function front_get($request, $db) {
-  print($_COOKIE['save']);
-  exit();
+
     $allowed_lang=getLangs($db);
     // Массив для временного хранения сообщений пользователю.
     $messages = array();
